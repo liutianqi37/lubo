b0c1007a71f4137d9b38b1631f61f092a417e8f8
liuqi <l289233@163.com>
2018-08-09 08:24:39 +0800

    sw

---
+++ day7/src/js/require.js
@@ -0,0 +1,12 @@
+function require(element, cb) {
+    console.log(element, cb())
+}
+require(['index'], function () {
+    console.log(1)
+})
+function define(element, cb) {
+    console.log(element, cb(4))
+}
+define(['swiper', 'jquery'], function (e) {
+    console.log(e)
+})
